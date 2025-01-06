e1dd44388f2195e7a10dde2ea1cedb493a006d8f
Shahin <shahinamman80@gmail.com>
2025-01-07 01:41:33 +0330

    nativewind configured

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-500 flex-1 items-center justify-center">
+      <Text className="text-white text-2xl text-center">
+        Edit app/fsda.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
